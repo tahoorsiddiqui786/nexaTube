0e5b41b278ea0a9470bf9b126eb95840dd988022
Tahoor Siddiqui <Tahoorsiddiqui@gmail.com>
2025-02-23 14:13:57 +0530

    models

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@
 // require('dotenv').config({  path : "./env"});
 import dotenv from "dotenv";
 import  connectDB  from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path : "./env"
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`App is listen at port ${process.env.PORT}`);
+    })
+}
+)
+.catch((error)=>{
+    console.log("error is :",error);
+}) ;
 
 // const app = express();
 
